a267e9e1109e4d7fbabc986c6907ae715243dade
Bahadır Kalaçay <36983849+bahadirkalacay@users.noreply.github.com>
2022-04-02 13:24:43 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./components/main.scss";
+
+const root= ReactDOM.createRoot(
+  document.getElementById("root"));
+  root.render(<App/>);
